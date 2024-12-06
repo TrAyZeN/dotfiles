f78e79c1713b8870e3aba1f176c4b2b400150269
TrAyZeN <1810leo@gmail.com>
2024-12-06 13:39:04 +0100

    nvim: Fix enable format after save

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -23,6 +23,9 @@ return {
                 lsp_format = "fallback",
                 timeout_ms = 500,
             },
+            format_after_save = {
+                lsp_format = "fallback",
+            },
         },
     },
     -- Display keys
